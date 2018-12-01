aa36ab54cd75596b8409705166228cf51a86ff3c
juraj222 <juraj.vrabec222@gmail.com>
2018-12-01 14:29:24 +0100

    read from firestore add to firestore problem se zobrazenim nacitaneho

--- app/src/main/java/com/mv/jura/firebase_example/adapters/ListAdapter.java
+++ app/src/main/java/com/mv/jura/firebase_example/adapters/ListAdapter.java
@@ -6,14 +6,26 @@ import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.PagerSnapHelper;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.SnapHelper;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.mv.jura.firebase_example.Item;
 import com.mv.jura.firebase_example.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import butterknife.ButterKnife;
 
@@ -21,10 +33,17 @@ import butterknife.ButterKnife;
 public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>{
     private ArrayList<Item> mItems = new ArrayList<>();
     private Context mContext;
+    private FirebaseFirestore db;
 
     public ListAdapter(Context context, ArrayList<Item> items) {
         mContext = context;
         mItems = items;
+        // Access a Cloud Firestore instance from your Activity
+        db = FirebaseFirestore.getInstance();
+        FirebaseFirestoreSettings settings = new FirebaseFirestoreSettings.Builder()
+                .setTimestampsInSnapshotsEnabled(true)
+                .build();
+        db.setFirestoreSettings(settings);
     }
     @Override
     public ListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
@@ -45,6 +64,7 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>{
             mItems.get(position).getVideoPlayer().release();
         }
         super.onViewRecycled(holder);
+
     }
 
     @Override
@@ -89,9 +109,51 @@ public class ListAdapter extends RecyclerView.Adapter<ListAdapter.ViewHolder>{
     //tu sa naplni profil a prispevky profilu
     private ArrayList<Item> populateItems() {
         ArrayList<Item> items = new ArrayList<>();
-        items.add(new Item("fero","10.1.2019", null,"5",null,null,true));
-        items.add(new Item("fero",null, "12.1.2019",null,null,"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",false));
-        items.add(new Item("fero",null, "13.1.2019",null,"http://i.imgur.com/e7MfwB0.jpg",null,false));
+//        items.add(new Item("fero","10.1.2019", null,"5",null,null,true));
+        //items.add(new Item("fero",null, "12.1.2019",null,null,"http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4",false));
+        //items.add(new Item("fero",null, "13.1.2019",null,"http://i.imgur.com/e7MfwB0.jpg",null,false));
+        items = getData();
+//        addItem(new Item("fero","10.1.2019", null,"5",null,null,true));
         return items;
+    }
+
+    private ArrayList<Item> getData(){
+        final ArrayList<Item> items = new ArrayList<>();
+        DocumentReference user = db.collection("users").document("Pokus");
+        user.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()){
+                    DocumentSnapshot doc = task.getResult();
+                    items.add(new Item( doc.getString("username"),doc.get("date").toString(), null,doc.get("numberOfPosts").toString(),null,null,true));
+                }
+            }
+        })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        e.printStackTrace();
+                    }
+                });
+
+        return items;
+    }
+
+    private void addItem(Item item){
+        Map<String, Object> newItem = new HashMap<>();
+        newItem.put("username", item.getName());
+        newItem.put("date", item.getDate());
+        newItem.put("numberOfPosts", item.getPostCount());
+        db.collection("users").document("Pokus").set(newItem)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                    }
+                });
     }
 }
