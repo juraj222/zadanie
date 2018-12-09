52fd4724616a924fc2aee24e5fbe4501c691e0a9
juraj222 <juraj.vrabec222@gmail.com>
2018-12-09 16:35:43 +0100

    add crashlatic ci co

--- app/src/main/java/com/mv/jura/firebase_example/LoginActivity.java
+++ app/src/main/java/com/mv/jura/firebase_example/LoginActivity.java
@@ -15,6 +15,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.crashlytics.android.Crashlytics;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
